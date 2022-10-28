09229c45eff36074c209a7ccca74379c459b7338
asmitakarmakar <asmitakarmakar21@gmail.com>
2022-10-28 15:58:13 -0500

    idk

--- app/src/main/java/com/example/squirrelrun/GameActivity.kt
+++ app/src/main/java/com/example/squirrelrun/GameActivity.kt
@@ -1,4 +1,12 @@
 package com.example.squirrelrun
 
-class GameActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+    //override
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game_page)
+    }
 }
