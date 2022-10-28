4265d8510a29e15f58a5f2124feaa8e357ceca1b
shvetkat <s.rajkatta02@gmail.com>
2022-10-27 20:16:39 -0500

    all navigation done

--- app/src/main/java/com/example/squirrelrun/GameActivity.kt
+++ app/src/main/java/com/example/squirrelrun/GameActivity.kt
@@ -1,17 +1,29 @@
 package com.example.squirrelrun
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.squirrelrun.databinding.GamePageBinding
+import com.example.squirrelrun.databinding.SettingsBinding
 
 class GameActivity : AppCompatActivity() {
 
     private lateinit var binding: GamePageBinding
-    private val resultList: ArrayList<String> = arrayListOf()
+    private lateinit var listIntent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = GamePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.settingsBtn.setOnClickListener { launchSettings() }
+        binding.homeBtn.setOnClickListener { launchHome() }
+    }
+    private fun launchSettings() {
+        listIntent = Intent(this, SettingsActivity::class.java)
+        startActivity(listIntent)
+    }
+    private fun launchHome() {
+        listIntent = Intent(this, MainActivity::class.java)
+        startActivity(listIntent)
     }
 }
--- app/src/main/java/com/example/squirrelrun/SettingsActivity.kt
+++ app/src/main/java/com/example/squirrelrun/SettingsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.squirrelrun
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.squirrelrun.databinding.SettingsBinding
@@ -7,11 +8,21 @@ import com.example.squirrelrun.databinding.SettingsBinding
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: SettingsBinding
-    private val resultList: ArrayList<String> = arrayListOf()
+    private lateinit var listIntent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = SettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.gameBtn.setOnClickListener { launchStart() }
+        binding.homeBtn.setOnClickListener { launchHome() }
+    }
+    private fun launchStart() {
+        listIntent = Intent(this, StartActivity::class.java)
+        startActivity(listIntent)
+    }
+    private fun launchHome() {
+        listIntent = Intent(this, MainActivity::class.java)
+        startActivity(listIntent)
     }
 }
