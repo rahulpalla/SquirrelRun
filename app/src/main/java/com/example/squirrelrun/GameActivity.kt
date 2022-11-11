ce7748c6c6173e2c204b41edcd64cffc8bb0e009
asmitakarmakar <asmitakarmakar21@gmail.com>
2022-11-11 12:42:22 -0600

    squirrel jumps and wolf moves across screen

--- app/src/main/java/com/example/squirrelrun/GameActivity.kt
+++ app/src/main/java/com/example/squirrelrun/GameActivity.kt
@@ -1,5 +1,8 @@
 package com.example.squirrelrun
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.app.Notification
 import android.content.Intent
 import android.graphics.Point
@@ -11,13 +14,14 @@ import android.view.View
 import android.view.WindowManager
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import androidx.core.view.GestureDetectorCompat
 import com.example.squirrelrun.databinding.GamePageBinding
+import kotlinx.coroutines.NonCancellable.start
 
 private const val DEBUG_TAG = "Gestures"
 
-class GameActivity : AppCompatActivity(), GestureDetector.OnGestureListener,
-    GestureDetector.OnDoubleTapListener {
+class GameActivity : AppCompatActivity() {
 
     private lateinit var binding: GamePageBinding
     private lateinit var listIntent: Intent
@@ -25,6 +29,8 @@ class GameActivity : AppCompatActivity(), GestureDetector.OnGestureListener,
 
 //    private lateinit var gameView: GameView
     private lateinit var squirrel: ImageView
+    private lateinit var wolf: ImageView
+    private lateinit var acorn: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,16 +42,12 @@ class GameActivity : AppCompatActivity(), GestureDetector.OnGestureListener,
 
         mDetector = GestureDetectorCompat(this, MyGestureListener())
 
-//        val point = Point()
-//        windowManager.defaultDisplay.getSize(point)
-//
-//        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//
-//        gameView = GameView(this, point.x, point.y)
-//
-//        setContentView(gameView)
         squirrel = findViewById(R.id.squirrel)
-//        onTouchEvent(MotionEvent.)
+        wolf = findViewById(R.id.wolf)
+        acorn = findViewById(R.id.acorn)
+
+        movingWolf()
+
     }
     private fun launchSettings() {
         listIntent = Intent(this, SettingsActivity::class.java)
@@ -57,6 +59,11 @@ class GameActivity : AppCompatActivity(), GestureDetector.OnGestureListener,
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                jump();
+            }
+        }
         mDetector.onTouchEvent(event)
         return super.onTouchEvent(event)
     }
@@ -72,14 +79,39 @@ class GameActivity : AppCompatActivity(), GestureDetector.OnGestureListener,
             return true
         }
     }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        gameView.pause()
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//        gameView.resume()
-//    }
+
+    private fun jump() {
+        val animator = ObjectAnimator.ofFloat(squirrel, View.TRANSLATION_Y, -500f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        disableDuringAnimation(animator)
+        animator.start()
+    }
+
+    private fun movingWolf() {
+        //randomly pick a starting x-coord place and make the wolf appear there
+        //slide it till just past the squirrel and once it hits that x coor then make it disapear
+//        val animator = ObjectAnimator.ofFloat(wolf, View.TRANSLATION_X, -1000f)
+        val animator = ObjectAnimator.ofFloat(wolf, "translationX", -4000f).apply {
+            duration = 4000
+            start()
+        }
+
+//        animator.repeatCount = 3
+        animator.doOnEnd { wolf.visibility = View.GONE }
+        disableDuringAnimation(animator)
+        animator.start()
+    }
+
+    private fun disableDuringAnimation(animator: ObjectAnimator) {
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+               //TODO: disable touch gesture
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                //TODO: enable touch gesture
+            }
+        })
+    }
 }
