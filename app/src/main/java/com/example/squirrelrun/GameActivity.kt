2537fdd9f7f3b47ff3b9be9d301263a3b08c9bea
asmitakarmakar <asmitakarmakar21@gmail.com>
2022-11-11 11:34:54 -0600

    attempting touch gesture

--- app/src/main/java/com/example/squirrelrun/GameActivity.kt
+++ app/src/main/java/com/example/squirrelrun/GameActivity.kt
@@ -1,18 +1,30 @@
 package com.example.squirrelrun
 
+import android.app.Notification
 import android.content.Intent
 import android.graphics.Point
 import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
 import android.view.WindowManager
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
 import com.example.squirrelrun.databinding.GamePageBinding
 
-class GameActivity : AppCompatActivity() {
+private const val DEBUG_TAG = "Gestures"
+
+class GameActivity : AppCompatActivity(), GestureDetector.OnGestureListener,
+    GestureDetector.OnDoubleTapListener {
 
     private lateinit var binding: GamePageBinding
     private lateinit var listIntent: Intent
+    private lateinit var mDetector: GestureDetectorCompat
 
 //    private lateinit var gameView: GameView
+    private lateinit var squirrel: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +34,8 @@ class GameActivity : AppCompatActivity() {
         binding.settingsBtn.setOnClickListener { launchSettings() }
         binding.homeBtn.setOnClickListener { launchHome() }
 
+        mDetector = GestureDetectorCompat(this, MyGestureListener())
+
 //        val point = Point()
 //        windowManager.defaultDisplay.getSize(point)
 //
@@ -30,6 +44,8 @@ class GameActivity : AppCompatActivity() {
 //        gameView = GameView(this, point.x, point.y)
 //
 //        setContentView(gameView)
+        squirrel = findViewById(R.id.squirrel)
+//        onTouchEvent(MotionEvent.)
     }
     private fun launchSettings() {
         listIntent = Intent(this, SettingsActivity::class.java)
@@ -38,6 +54,23 @@ class GameActivity : AppCompatActivity() {
     private fun launchHome() {
         listIntent = Intent(this, MainActivity::class.java)
         startActivity(listIntent)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        mDetector.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
+    private class MyGestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onDown(event: MotionEvent): Boolean {
+            Log.d(DEBUG_TAG, "onDown: $event")
+            return true
+        }
+
+        override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
+            return true
+        }
     }
 //
 //    override fun onPause() {
