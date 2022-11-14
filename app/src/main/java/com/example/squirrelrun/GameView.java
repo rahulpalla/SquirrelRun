4dbf88654af20f22a898b9d2a0c5325e321c969a
rahulpalla <rahulpalla@utexas.edu>
2022-11-13 20:59:09 -0600

    implicit intent

--- app/src/main/java/com/example/squirrelrun/GameView.java
+++ app/src/main/java/com/example/squirrelrun/GameView.java
@@ -8,7 +8,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
-import android.graphics.Typeface;
 import android.os.Handler;
 import android.view.Display;
 import android.view.MotionEvent;
@@ -56,9 +55,7 @@ public class GameView extends View {
         handler = new Handler();
         background = BitmapFactory.decodeResource(context.getResources(), R.drawable.background_home);
         scorePaint = new Paint();
-        Typeface font = Typeface.createFromAsset(getContext().getAssets(), "font/sporto.ttf");
-        scorePaint.setTypeface(font);
-        scorePaint.setColor(Color.WHITE);
+        scorePaint.setColor(Color.BLACK);
         scorePaint.setTextSize(TEXT_SIZE);
         scorePaint.setTextAlign(Paint.Align.LEFT);
 
--- app/src/main/java/com/example/squirrelrun/SettingsActivity.kt
+++ app/src/main/java/com/example/squirrelrun/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.squirrelrun
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.squirrelrun.databinding.SettingsBinding
@@ -17,6 +18,21 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.gameBtn.setOnClickListener { launchStart() }
         binding.homeBtn.setOnClickListener { launchHome() }
+        val webIntent: Intent = Uri.parse("https://docs.google.com/forms/d/e/1FAIpQLScvEchFvcV3nsaqBkX5kmHwAz1ZNGmEJlh5lOoZZ8eqr4F3PA/viewform?usp=sf_link").let { webpage ->
+            Intent(Intent.ACTION_VIEW, webpage)
+        }
+        binding.feedbackButton.setOnClickListener {
+            launchLauncher(webIntent)
+        }}
+
+
+//    private fun openWeb() {
+//        val webIntent: Intent = Uri.parse("https://docs.google.com/forms/d/e/1FAIpQLScvEchFvcV3nsaqBkX5kmHwAz1ZNGmEJlh5lOoZZ8eqr4F3PA/viewform?usp=sf_link").let { webpage ->
+//            Intent(Intent.ACTION_VIEW, webpage)
+//        }
+//    }
+    private fun launchLauncher(webIntent: Intent) {
+        startActivity(webIntent)
     }
     private fun launchStart() {
         listIntent = Intent(this, StartActivity::class.java)
