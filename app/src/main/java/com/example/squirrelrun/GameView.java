df77eebc34e441035bab58d56eef0caddc866c72
asmitakarmakar <asmitakarmakar21@gmail.com>
2022-11-13 21:46:28 -0600

    game over page not working

--- app/src/main/java/com/example/squirrelrun/GameView.java
+++ app/src/main/java/com/example/squirrelrun/GameView.java
@@ -2,6 +2,7 @@ package com.example.squirrelrun;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -21,6 +22,7 @@ public class GameView extends View {
     Handler handler;
     static int screenWidth, screenHeight;
     int points = 0;
+    int life = 1;
     boolean isPlaying = true;
     Paint scorePaint;
     int TEXT_SIZE = 100;
@@ -65,9 +67,15 @@ public class GameView extends View {
         canvas.drawBitmap(background, 0, 0, null);
         canvas.drawText("Points: " + points, 0, TEXT_SIZE, scorePaint);
 
-        if (!isPlaying) {
-            //launch game over screen
-        }
+//        if (!isPlaying) {
+//            //launch game over screen
+//            paused = true;
+//            handler = null;
+//            Intent intent = new Intent(context, GameOverActivity.class);
+//            intent.putExtra("points", points);
+//            context.startActivity(intent);
+//            ((Activity) context).finish();
+//        }
 
         if (wolfFalling == false) {
             Wolf wolf = new Wolf(context, random.nextInt(1200), 0);
@@ -90,7 +98,15 @@ public class GameView extends View {
                     && wolves.get(i).x <= squirrel.x + squirrel.getWidth()
                     && wolves.get(i).y >= squirrel.y
                     && wolves.get(i).y <= screenHeight){
-                isPlaying = false;
+
+                    //launch game over screen
+                    paused = true;
+                    handler = null;
+                    Intent intent = new Intent(context, GameOverActivity.class);
+                    intent.putExtra("points", points);
+                    context.startActivity(intent);
+                    ((Activity) context).finish();
+
                 wolves.remove(i);
             } else if(wolves.get(i).y >= screenHeight){
                 wolves.remove(i);
