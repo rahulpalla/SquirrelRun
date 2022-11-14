fc13d1ee5a0d87c9947a69b56e7bb89610784332
asmitakarmakar <asmitakarmakar21@gmail.com>
2022-11-14 14:34:01 -0600

    included home button

--- app/src/main/java/com/example/squirrelrun/GameOverActivity.kt
+++ app/src/main/java/com/example/squirrelrun/GameOverActivity.kt
@@ -17,12 +17,17 @@ class GameOverActivity : AppCompatActivity() {
         binding = GameOverBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.playGameBtn.setOnClickListener { launchGame() }
+        binding.homeBtn.setOnClickListener { launchMain() }
         val point = intent.getIntExtra("points", 0)
         binding.points.text = resources.getString(R.string.points, point)
 
     }
     private fun launchGame() {
         listIntent = Intent(this, GameActivity::class.java)
+        startActivity(listIntent)
+    }
+    private fun launchMain() {
+        listIntent = Intent(this, MainActivity::class.java)
         startActivity(listIntent)
     }
 }
