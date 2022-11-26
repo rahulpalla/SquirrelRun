68e7e766a307fb5f23982785c5a9d0363aeeb40a
asmitakarmakar <asmitakarmakar21@gmail.com>
2022-11-26 16:24:39 -0600

    testing stuff

--- app/src/main/java/com/example/squirrelrun/Squirrel.java
+++ app/src/main/java/com/example/squirrelrun/Squirrel.java
@@ -3,6 +3,9 @@ package com.example.squirrelrun;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.DisplayMetrics;
 
 import java.util.Random;
 
@@ -16,6 +19,16 @@ public class Squirrel {
     public Squirrel(Context context) {
         this.context = context;
         scaler = BitmapFactory.decodeResource(context.getResources(), R.drawable.squirrel_img);
+
+//        DisplayMetrics dm = new DisplayMetrics();`  `
+//        context.getWindowManager().getDefaultDisplay().getMetrics(dm);
+//        float densityScale = dm.density;
+//        float scaledWidth = 300 * densityScale;
+//        float scaledHeight = 300 * densityScale;
+
+//        squirrel = Bitmap.createScaledBitmap(scaler, (int)scaledWidth, (int)scaledHeight, false);
+
+
         squirrel = Bitmap.createScaledBitmap(scaler, 300, 300, false);
         random = new Random();
         x = random.nextInt(GameView.screenWidth);
